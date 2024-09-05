7bc374500412452cbf6b446fc3e4624b7bfae570
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-09-05 20:28:33 -0300

    create payload and start request

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -30,8 +33,41 @@ func main() {
 		return
 	}
 
-var value string
-fmt.Print("Enter a monetary value: ")
-fmt.Scanln(&value)
+	var value string
+	fmt.Print("Enter a monetary value: ")
+	fmt.Scanln(&value)
 
+	//Create payload
+	requestBody, err := json.Marshal(GTPRequest{
+		Messages: []GTPMessage{
+			{Role: "user", Content: fmt.Sprintf("Write the value %s in full.", value)},
+		},
+		Model: "gpt-3.5-turbo",
+	})
+	if err != nil {
+		fmt.Printf("Error creating request body: %v\n", err)
+		return
+	}
+
+	//Create request
+	req, err := http.NewRequest("POST", apiURl, bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		return
+	}
+
+
+	//Add headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+
+	//ChatGPT api key
+	client := &http.Client{}
+	resp, err := client.Do(req) //Send request
+	if err != nil {
+		fmt.Printf("Error sending request: %v\n", err)
+		return
+	}
+
+	defer resp.Body.Close()
 }
